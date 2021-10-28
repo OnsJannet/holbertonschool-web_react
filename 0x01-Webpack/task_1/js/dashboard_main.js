12872a90235c69f82988dbad33d5f5036d1925ec
onsjannet <onsjannet@gmail.com>
2021-10-28 20:14:06 +0100

    task1

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,20 @@
 import $ from "jquery";
-import _ from 'lodash';
+const _ = require('lodash');
 
-$('body').append("<p>Holberton Dashboard</p>");
-$('body').append("<p>Dashboard data for the studnts</p>");
-$('body').append("<button>Click here to get started</button>");
-$('body').append("<p id='count'></p>");
-$('body').append("<p>Copyright - Holberton School</p>");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id=\'count\'></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+$('button').on('click', _.debounce(updateCounter, 500, {
+  'leading': true,
+  'trailing': false,
+}));
 
 let count = 0;
-  
+
 function updateCounter() {
   count = count + 1;
-  $('#count').text(`${count.toString()} clicks on the button`);
+  $('#count').text(`${count} clicks on the button`);
 }
-
-$('button').on('click', _.debounce(updateCounter));
